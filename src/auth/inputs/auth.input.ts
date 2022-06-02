63cf8a10305fdbce5b2f9c93a298291fd0e76bcf
Vladislav Oliinyk <catdev.dev@gmail.com>
2022-06-02 04:39:38 +0300

    added

--- src/auth/inputs/auth.input.ts
+++ src/auth/inputs/auth.input.ts
@@ -1,26 +1,41 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsLowercase,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
 
 @InputType()
 export class RegisterReq {
+  @MinLength(7)
+  @MaxLength(30)
   @IsNotEmpty()
-  email: string;
-
+  @IsLowercase()
+  private email: string;
+  @MinLength(6)
+  @MaxLength(30)
   @IsNotEmpty()
-  username: string;
-
-  @IsNotEmpty()
+  @IsLowercase()
   password: string;
+  @MinLength(3)
+  @MaxLength(30)
+  @IsNotEmpty()
+  @IsLowercase()
+  username: string;
 }
 
 @InputType()
 export class AuthReq {
   @IsNotEmpty()
+  @IsLowercase()
+  password: string;
+  @IsNotEmpty()
+  @IsLowercase()
   User_id: number;
 
   @IsNotEmpty()
+  @IsLowercase()
   username: string;
-
-  @IsNotEmpty()
-  password: string;
 }
